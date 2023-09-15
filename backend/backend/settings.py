1c0d0cb1ae3846e2efc70f3b74c874afbef52dfa
zahara-Rahmanchy <zaharachy72@gmail.com>
2023-09-15 18:23:40 +0600

    requirements.txt updated

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,11 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-h(y@q-*@@)r%5i6c8)92!ynj1alh8#&83jwkb$0o3vq)uq%-aj"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
-# ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
